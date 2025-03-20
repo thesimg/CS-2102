e7c72c9dfc10c6a226f54c0221a5a43e45ceec8f
thesimg <graham.simons05@gmail.com>
2025-03-20 12:50:44 -0400

    Debugging and helper methods, w javadoc documentation

--- 2025-03-20 Lecture2/Examples.java
+++ 2025-03-20 Lecture2/Examples.java
@@ -1,6 +1,9 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+/**
+ * @author graham simons
+ */
 public class Examples {
 
     @Test
@@ -12,5 +15,23 @@ public class Examples {
     public void testTicketFasterWithSeniorDiscount(){
         TicketFaster ticketFaster = new TicketFaster();
         assertEquals(16.5,ticketFaster.priceForTickets(3,true), 0.01);
+    }
+
+    @Test
+    public void testMarqueeNoTicketsRemaining(){
+        TicketFaster ticketFaster = new TicketFaster();
+        assertEquals("Now Playing: Hatsune Miku !SOLD OUT!", ticketFaster.marquee("Hatsune Miku",0));
+    }
+
+    @Test
+    public void testMarqueeTicketsRemaining(){
+        TicketFaster ticketFaster = new TicketFaster();
+        assertEquals("Now Playing: Dingus9000 only 68 tickets left", ticketFaster.marquee("Dingus9000",68));
+    }
+
+    @Test
+    public void testTicketsLeft(){
+        TicketFaster ticketFaster = new TicketFaster();
+        assertEquals("only 68 tickets left", ticketFaster.ticketsLeft(68));
     }
 }
--- 2025-03-20 Lecture2/TicketFaster.java
+++ 2025-03-20 Lecture2/TicketFaster.java
@@ -15,4 +15,24 @@ public class TicketFaster {
         }
     }
 
+    /**
+     * Generates a marquee string
+     * @param concert is the name of the performer
+     * @param numTickets >= 0
+     * @return a Now playing string, says if sold out: "Now Playing %s %s"
+     */
+    public String marquee(String concert, int numTickets){
+//        return String.format("Now Playing: %s %s", concert, (numTickets > 0) ? numTickets + " tickets left" : "!SOLD OUT!");
+        return String.format("Now Playing: %s %s", concert, ticketsLeft(numTickets));
+    }
+
+    /**
+     * Generates a string describing how many tickets are left
+     * @param numTickets is the number of tickets left
+     * @return a string saying tickets left or sold out
+     */
+    protected String ticketsLeft(int numTickets) {
+        return (numTickets > 0) ? String.format("only %d tickets left", numTickets) : "!SOLD OUT!";
+    }
+
 }
