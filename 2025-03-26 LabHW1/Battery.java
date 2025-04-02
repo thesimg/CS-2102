6c1dcacaf0511bc6063d0b49707797c0d1b43bb2
Graham Simons <graham.simons05@gmail.com>
2025-04-01 23:59:04 -0400

    2025-04-01 HW01, failed doesReachDest() test unfortunately

--- 2025-03-26 LabHW1/Battery.java
+++ 2025-03-26 LabHW1/Battery.java
@@ -1,3 +1,7 @@
+/**
+ * a Battery object, used in a MAV
+ * @author graham simons
+ */
 public class Battery {
     /**
      * a battery object
@@ -10,5 +14,37 @@ public class Battery {
     public Battery(double capacity, double amountLeft){
         this.capacity = capacity;
         this.amountLeft = amountLeft;
+    }
+
+    /**
+     * @param currentDraw takes in the total current draw of the MAV's propellers
+     * @return the time that the MAV can run based on the battery's amount left
+     */
+    protected double timeToEmpty(double currentDraw){
+        return this.amountLeft / currentDraw;
+    }
+
+    /**
+     * @param currentDraw takes in the total current draw of the MAV's propellers
+     * @return the max amount of time that the MAV can fly based on the battery's fully charged capacity
+     */
+    protected double maxTimeToEmpty(double currentDraw){
+        return this.capacity / currentDraw;
+    }
+
+    /**
+     * @param currentDraw the total current draw of the MAV (mA)
+     * @param time the total time the MAV is being run for
+     * @return the battery amount discharged in the given time
+     */
+    protected double amountDischargedIn(double currentDraw, double time){
+        return currentDraw * time;
+    }
+
+    /**
+     * @param amount decreases the battery's current power by amount (mAs)
+     */
+    protected void dischargeBy(double amount){
+        this.amountLeft -= amount;
     }
 }
--- 2025-03-26 LabHW1/MAV.java
+++ 2025-03-26 LabHW1/MAV.java
@@ -1,4 +1,15 @@
+/**
+ * a Micro Air Vehicle object, which has n propellers and a battery
+ * @author graham simons
+ */
 public class MAV {
+    /**
+     * a MAV object
+     * @param name the name of the MAV
+     * @param propellers is the Propellers object for the MAV
+     * @param battery is the Battery object for the MAV
+     * @param metersToDest the distance to the destination in meters
+     */
     String name;
     Propellers propellers;
     Battery battery;
@@ -15,14 +26,19 @@ public class MAV {
      * @return the ratio (between 0.0 and 1.0) of meters travelable on the current charge to meters until its destination. If the vehicle can go past its destination, this should return 1.0
      */
     public double percentUntilRecharge(){
-        return 0;
+        double currentDraw = this.propellers.totalCurrentDraw();
+
+        double batteryTimeLeft = this.battery.timeToEmpty(currentDraw);
+        double metersTravelable = propellers.distanceTravelable(batteryTimeLeft);
+
+        return metersTravelable / metersToDest;
     }
 
     /**
      * @return if the vehicle can reach its destination with its current charge
      */
     public boolean doesReachDest(){
-        return false;
+        return this.percentUntilRecharge() == 1.0;
     }
 
     /**
@@ -30,7 +46,22 @@ public class MAV {
      * @return the name of whichever vehicle can go further on its full battery capacity. if there is a tie (within 0.01 meters), it produces the names in this format: “thisVehiclesName&paramVehiclesName”
      */
     public String whichGoesFurther(MAV otherMAV){
-        return "";
+        double thisMaxTimeToEmpty = this.battery.maxTimeToEmpty(this.propellers.totalCurrentDraw());
+        double thisMaxDistanceTravelable = this.propellers.distanceTravelable(thisMaxTimeToEmpty);
+
+        double otherMaxTimeToEmpty = otherMAV.battery.maxTimeToEmpty(otherMAV.propellers.totalCurrentDraw());
+        double otherMaxDistanceTravelable = otherMAV.propellers.distanceTravelable(otherMaxTimeToEmpty);
+
+
+        double difference = thisMaxDistanceTravelable - otherMaxDistanceTravelable;
+
+        if (Math.abs(difference) <= 0.01){
+            return this.name + "&" + otherMAV.name;
+        } else if (difference < 0){ // other was larger
+            return otherMAV.name;
+        } else {
+            return this.name;
+        }
     }
 
     /**
@@ -38,6 +69,15 @@ public class MAV {
      * @param seconds
      */
     public void flyFor(double seconds){
+        double totalCurrentDraw = this.propellers.totalCurrentDraw();
+        double batteryCapacityUsed = this.battery.amountDischargedIn(totalCurrentDraw, seconds);
+
+        this.battery.dischargeBy((this.battery.amountLeft - batteryCapacityUsed >= 0) ? batteryCapacityUsed : this.battery.amountLeft);
+
+
+        double distanceTravelled = propellers.distanceTravelable(seconds);
+
+        this.metersToDest = (this.metersToDest - distanceTravelled >= 0) ? this.metersToDest - distanceTravelled : 0;
 
     }
 }
